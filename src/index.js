73c035f304d4708a5afc0acd659a752016bb3861
AsemBq <asem.b7460611@gmail.com>
2024-06-06 11:52:37 +0330

    StopWatch

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css'
+
+import App from "./App"
+import {createRoot} from "react-dom/client";
+
+createRoot(document.getElementById('root')).render(<App/>);
+
+
+
